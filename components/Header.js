eb1d8a646c916206aa088aeed1ee8ad6e920cb71
Ashwin Khode <ashwin.khode1@gmail.com>
2021-05-04 06:45:10 +0530

    add: UI components

---
+++ components/Header.js
@@ -0,0 +1,50 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+import Nav from './Nav';
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  position: relative;
+  z-index: 2;
+  margin-left: 2rem;
+  transform: skew(-7deg);
+  background: red;
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
+
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1fr solid var(--black, black);
+  }
+`;
+
+const Header = () => (
+  <HeaderStyles>
+    <div className="bar">
+      <Logo>
+        <Link href="/">Sick Fits</Link>
+      </Logo>
+    </div>
+    <div className="sub-bar">
+      <p>Search</p>
+    </div>
+    <Nav />
+  </HeaderStyles>
+);
+
+export default Header;
---
+++ components/Page.js
@@ -0,0 +1,70 @@
+import PropTypes from 'prop-types';
+import styled, { createGlobalStyle } from 'styled-components';
+import Header from './Header';
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.wolf2') format('wolf2');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  html {
+    --red: #ff0000;
+    --black: #393939;
+    --grey: #3a3a3a;
+    --gray: var(--grey);
+    --lightGrey: #e1e1e1;
+    --lightGray: var(--lightGrey);
+    --offWhite: #ededed;
+    --maxWidth: 1000px;
+    --bs: 0 12px 24px 0 rgba(0,0,0,.09);
+    box-sizing: border-box;
+  }
+
+  *, *::after, *::before {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    margin: 0;
+    padding: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--black);
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+
+  button {
+    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+const Page = ({ children, cool = 'Hello there' }) => (
+  <div>
+    <GlobalStyles />
+    <Header />
+    <InnerStyles>{children}</InnerStyles>
+  </div>
+);
+
+Page.propTypes = {
+  cool: PropTypes.string,
+  children: PropTypes.any,
+};
+
+export default Page;
