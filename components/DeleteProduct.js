72c8de8babed50f69766381a77a14d450197d82c
Ashwin Khode <ashwin.khode1@gmail.com>
2021-05-08 07:32:58 +0530

    add: delete product

---
+++ components/DeleteProduct.js
@@ -0,0 +1,42 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+
+/* eslint-disable react/prop-types */
+const DELETE_PRODUCT_MUTATION = gql`
+  mutation DELETE_PRODUCT_MUTATION($id: ID!) {
+    deleteProduct(id: $id) {
+      id
+      title
+    }
+  }
+`;
+
+function update(cache, payload) {
+  cache.evict(cache.identify(payload.data.deleteProduct));
+}
+
+export default function DeleteProduct({ id, children }) {
+  const [deleteProduct, { loading, error }] = useMutation(
+    DELETE_PRODUCT_MUTATION,
+    {
+      variables: {
+        id,
+      },
+      update,
+    }
+  );
+  return (
+    <button
+      type="button"
+      disabled={loading}
+      onClick={() => {
+        // eslint-disable-next-line no-restricted-globals
+        if (confirm('Are you sure you want to delete this item?')) {
+          deleteProduct().catch((err) => alert(err.message));
+        }
+      }}
+    >
+      {children}
+    </button>
+  );
+}
