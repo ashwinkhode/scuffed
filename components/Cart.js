5ee0e8afff1d570c4740b1d836efbdbd9af4c98e
Ashwin Khode <ashwin.khode1@gmail.com>
2021-06-20 21:52:36 +0530

    add: checkout feature

--- components/Cart.js
+++ components/Cart.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import calcTotalPrice from '../lib/calcTotalPrice';
 import { useCart } from '../lib/cartState';
 import formatMoney from '../lib/formatMoney';
+import CheckoutForm, { Checkout } from './Checkout';
 import RemoveFromCart from './RemoveFromCart';
 import CartStyles from './styles/CartStyles';
 import CloseButton from './styles/CloseButton';
@@ -65,6 +66,7 @@ export default function Cart() {
       </ul>
       <footer>
         <p>{formatMoney(calcTotalPrice(me.cart))}</p>
+        <Checkout />
       </footer>
     </CartStyles>
   );
